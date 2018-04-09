1822b8b84ade376bc1f0de755e03305120f1d561
fuxingjfl <1119832643@qq.com>
2018-04-09 12:07:40 +0800

    Merge branch 'master' of E:\ZhiHuiYanjiao with conflicts.

---
+++ app/src/main/java/zhyj/dqjt/com/zhihuiyanjiao/IssueReportActivity.java
@@ -0,0 +1,10 @@
+package zhyj.dqjt.com.zhihuiyanjiao;
+
+import zhyj.dqjt.com.zhihuiyanjiao.base.BaseActivity;
+
+/**
+ * Created by Administrator on 2018/4/8.
+ */
+
+public class IssueReportActivity extends BaseActivity {
+}
